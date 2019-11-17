8b55d5583264a58913ae8d94c116f03dd17cc1ce
Surya Kanta De <surya.kanta@coviam.com>
2019-11-17 18:00:02 +0530

    bean added

--- src/main/java/com/shortener/urlshortener/common/config/BeanConfig.java
+++ src/main/java/com/shortener/urlshortener/common/config/BeanConfig.java
@@ -3,6 +3,7 @@ package com.shortener.urlshortener.common.config;
 
 import com.shortener.urlshortener.v1.enums.ServiceType;
 import com.shortener.urlshortener.v1.service.UrlShortenerService;
+import com.shortener.urlshortener.v1.service.impl.UrlShortenerMongoServiceImpl;
 import com.shortener.urlshortener.v1.service.impl.UrlShortenerPostgresqlServiceImpl;
 import com.shortener.urlshortener.v1.service.impl.UrlShortenerRedisServiceImpl;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,10 +21,13 @@ public class BeanConfig {
       @Qualifier("com.shortener.urlshortener.v1.service.impl.UrlShortenerRedisServiceImpl")
       UrlShortenerRedisServiceImpl urlShortenerRedisServiceImpl,
       @Qualifier("com.shortener.urlshortener.v1.service.impl.UrlShortenerPostgresqlServiceImpl")
-      UrlShortenerPostgresqlServiceImpl urlShortenerPostgresqlServiceImpl){
+      UrlShortenerPostgresqlServiceImpl urlShortenerPostgresqlServiceImpl,
+      @Qualifier("com.shortener.urlshortener.v1.service.impl.UrlShortenerMongoServiceImpl")
+      UrlShortenerMongoServiceImpl urlShortenerMongoServiceImpl){
     Map<ServiceType, UrlShortenerService> shortenerServiceMap = new HashMap<>();
     shortenerServiceMap.put(ServiceType.REDIS, urlShortenerRedisServiceImpl);
     shortenerServiceMap.put(ServiceType.POSTGRESQL, urlShortenerPostgresqlServiceImpl);
+    shortenerServiceMap.put(ServiceType.MONGO, urlShortenerMongoServiceImpl);
     return shortenerServiceMap;
   }
 }
